bb8dd4492d1e2356b1da84a4ca9211e56ecf2da9
Root <test@xyz.com>
2017-12-16 22:02:30 +0530

    kafka version degraded to 8

--- capstone-kafka-producer/src/main/java/com/edureka/kafka/configurations/KafkaProducerConfiguration.java
+++ capstone-kafka-producer/src/main/java/com/edureka/kafka/configurations/KafkaProducerConfiguration.java
@@ -35,11 +35,11 @@ public class KafkaProducerConfiguration {
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrap());
 		props.put(ProducerConfig.ACKS_CONFIG, kafkaProperties.getAcks());
 		props.put(ProducerConfig.RETRIES_CONFIG, kafkaProperties.getRetries());
-		props.put("partitioner.class", "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
-		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("partitioner.class", "kafka.producer.DefaultPartitioner");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 		Producer<String, String> producer = new KafkaProducer<String, String>(props);
-		logger.info("Balance Event Producer created.");
+		logger.info("Event Producer created.");
 		return producer;
 	}
 }
